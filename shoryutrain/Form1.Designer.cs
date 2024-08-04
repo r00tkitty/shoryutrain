976d40543b0f5975577ad6854dddb2fb4205610e
g20mi <g20mi@PORTAL3006>
2024-08-04 02:03:28 +0200

    Add project files.

---
+++ shoryutrain/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace shoryutrain
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ToolStripMenuItem inputHandlerSelector;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            xinputMenuItem = new ToolStripMenuItem();
+            directInputMenuItem = new ToolStripMenuItem();
+            buttonUpLeft = new Button();
+            buttonUp = new Button();
+            buttonUpRight = new Button();
+            buttonLeft = new Button();
+            buttonNeutral = new Button();
+            buttonRight = new Button();
+            buttonDownLeft = new Button();
+            buttonDown = new Button();
+            buttonDownRight = new Button();
+            menuStrip1 = new MenuStrip();
+            gameTypeSelector = new ToolStripMenuItem();
+            streetFighterToolStripMenuItem = new ToolStripMenuItem();
+            About = new ToolStripMenuItem();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            connectedControllerName = new TextBox();
+            inputHandlerSelector = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // inputHandlerSelector
+            // 
+            inputHandlerSelector.DropDownItems.AddRange(new ToolStripItem[] { xinputMenuItem, directInputMenuItem });
+            inputHandlerSelector.Name = "inputHandlerSelector";
+            inputHandlerSelector.Size = new Size(72, 20);
+            inputHandlerSelector.Text = "Controller";
+            inputHandlerSelector.Click += toolStripMenuItem1_Click;
+            // 
+            // xinputMenuItem
+            // 
+            xinputMenuItem.Checked = true;
+            xinputMenuItem.CheckState = CheckState.Checked;
+            xinputMenuItem.Name = "xinputMenuItem";
+            xinputMenuItem.Size = new Size(133, 22);
+            xinputMenuItem.Text = "Xinput";
+            xinputMenuItem.Click += xinputMenuItem_Click;
+            // 
+            // directInputMenuItem
+            // 
+            directInputMenuItem.Name = "directInputMenuItem";
+            directInputMenuItem.Size = new Size(133, 22);
+            directInputMenuItem.Text = "DirectInput";
+            directInputMenuItem.Click += directInputMenuItem_Click;
+            // 
+            // buttonUpLeft
+            // 
+            buttonUpLeft.Font = new Font("Segoe UI", 40F);
+            buttonUpLeft.Location = new Point(13, 27);
+            buttonUpLeft.Margin = new Padding(4, 3, 4, 3);
+            buttonUpLeft.Name = "buttonUpLeft";
+            buttonUpLeft.Size = new Size(100, 100);
+            buttonUpLeft.TabIndex = 0;
+            buttonUpLeft.Text = "🢄";
+            buttonUpLeft.UseVisualStyleBackColor = true;
+            // 
+            // buttonUp
+            // 
+            buttonUp.Font = new Font("Segoe UI", 40F);
+            buttonUp.Location = new Point(119, 27);
+            buttonUp.Margin = new Padding(4, 3, 4, 3);
+            buttonUp.Name = "buttonUp";
+            buttonUp.Size = new Size(100, 100);
+            buttonUp.TabIndex = 1;
+            buttonUp.Text = "🢁";
+            buttonUp.TextAlign = ContentAlignment.TopCenter;
+            buttonUp.UseVisualStyleBackColor = true;
+            // 
+            // buttonUpRight
+            // 
+            buttonUpRight.Font = new Font("Segoe UI", 40F);
+            buttonUpRight.Location = new Point(225, 27);
+            buttonUpRight.Margin = new Padding(4, 3, 4, 3);
+            buttonUpRight.Name = "buttonUpRight";
+            buttonUpRight.Size = new Size(100, 100);
+            buttonUpRight.TabIndex = 2;
+            buttonUpRight.Text = "🢅";
+            buttonUpRight.UseVisualStyleBackColor = true;
+            // 
+            // buttonLeft
+            // 
+            buttonLeft.Font = new Font("Segoe UI", 40F);
+            buttonLeft.Location = new Point(13, 133);
+            buttonLeft.Margin = new Padding(4, 3, 4, 3);
+            buttonLeft.Name = "buttonLeft";
+            buttonLeft.Size = new Size(100, 100);
+            buttonLeft.TabIndex = 3;
+            buttonLeft.Text = "🢀";
+            buttonLeft.UseVisualStyleBackColor = true;
+            // 
+            // buttonNeutral
+            // 
+            buttonNeutral.Font = new Font("Calibri", 39.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            buttonNeutral.Location = new Point(119, 133);
+            buttonNeutral.Margin = new Padding(4, 3, 4, 3);
+            buttonNeutral.Name = "buttonNeutral";
+            buttonNeutral.Size = new Size(100, 100);
+            buttonNeutral.TabIndex = 4;
+            buttonNeutral.Text = "Ⓝ";
+            buttonNeutral.UseVisualStyleBackColor = true;
+            // 
+            // buttonRight
+            // 
+            buttonRight.Font = new Font("Segoe UI", 40F);
+            buttonRight.Location = new Point(225, 133);
+            buttonRight.Margin = new Padding(4, 3, 4, 3);
+            buttonRight.Name = "buttonRight";
+            buttonRight.Size = new Size(100, 100);
+            buttonRight.TabIndex = 5;
+            buttonRight.Text = "🢂";
+            buttonRight.UseVisualStyleBackColor = true;
+            // 
+            // buttonDownLeft
+            // 
+            buttonDownLeft.Font = new Font("Segoe UI", 40F);
+            buttonDownLeft.Location = new Point(13, 239);
+            buttonDownLeft.Margin = new Padding(4, 3, 4, 3);
+            buttonDownLeft.Name = "buttonDownLeft";
+            buttonDownLeft.Size = new Size(100, 100);
+            buttonDownLeft.TabIndex = 6;
+            buttonDownLeft.Text = "🢇";
+            buttonDownLeft.UseVisualStyleBackColor = true;
+            // 
+            // buttonDown
+            // 
+            buttonDown.BackgroundImageLayout = ImageLayout.None;
+            buttonDown.Font = new Font("Segoe UI", 40F);
+            buttonDown.ImageAlign = ContentAlignment.BottomCenter;
+            buttonDown.Location = new Point(119, 239);
+            buttonDown.Margin = new Padding(4, 3, 4, 3);
+            buttonDown.Name = "buttonDown";
+            buttonDown.Size = new Size(100, 100);
+            buttonDown.TabIndex = 7;
+            buttonDown.Text = "🢃";
+            buttonDown.TextAlign = ContentAlignment.BottomCenter;
+            buttonDown.UseVisualStyleBackColor = true;
+            // 
+            // buttonDownRight
+            // 
+            buttonDownRight.BackgroundImageLayout = ImageLayout.None;
+            buttonDownRight.Font = new Font("Segoe UI", 40F);
+            buttonDownRight.Location = new Point(225, 239);
+            buttonDownRight.Margin = new Padding(4, 3, 4, 3);
+            buttonDownRight.Name = "buttonDownRight";
+            buttonDownRight.Size = new Size(100, 100);
+            buttonDownRight.TabIndex = 8;
+            buttonDownRight.Text = "🢆";
+            buttonDownRight.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { inputHandlerSelector, gameTypeSelector, About });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(718, 24);
+            menuStrip1.TabIndex = 9;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // gameTypeSelector
+            // 
+            gameTypeSelector.DropDownItems.AddRange(new ToolStripItem[] { streetFighterToolStripMenuItem });
+            gameTypeSelector.Name = "gameTypeSelector";
+            gameTypeSelector.Size = new Size(50, 20);
+            gameTypeSelector.Text = "Game";
+            // 
+            // streetFighterToolStripMenuItem
+            // 
+            streetFighterToolStripMenuItem.Name = "streetFighterToolStripMenuItem";
+            streetFighterToolStripMenuItem.Size = new Size(144, 22);
+            streetFighterToolStripMenuItem.Text = "Street Fighter";
+            // 
+            // About
+            // 
+            About.DisplayStyle = ToolStripItemDisplayStyle.Text;
+            About.Name = "About";
+            About.Size = new Size(52, 20);
+            About.Text = "About";
+            About.Click += About_Click;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Right;
+            button1.Location = new Point(469, 96);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 75);
+            button1.TabIndex = 10;
+            button1.Text = "LP";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Right;
+            button2.Location = new Point(550, 96);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 75);
+            button2.TabIndex = 11;
+            button2.Text = "MP";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Right;
+            button3.Location = new Point(631, 96);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 75);
+            button3.TabIndex = 12;
+            button3.Text = "HP";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Right;
+            button4.Location = new Point(469, 177);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 75);
+            button4.TabIndex = 13;
+            button4.Text = "LK";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Right;
+            button5.Location = new Point(550, 177);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 75);
+            button5.TabIndex = 14;
+            button5.Text = "MK";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Anchor = AnchorStyles.Right;
+            button6.Location = new Point(631, 177);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 75);
+            button6.TabIndex = 15;
+            button6.Text = "HK";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // connectedControllerName
+            // 
+            connectedControllerName.BorderStyle = BorderStyle.FixedSingle;
+            connectedControllerName.Location = new Point(13, 359);
+            connectedControllerName.Name = "connectedControllerName";
+            connectedControllerName.ReadOnly = true;
+            connectedControllerName.Size = new Size(312, 23);
+            connectedControllerName.TabIndex = 16;
+            connectedControllerName.TextChanged += connectedControllerName_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(718, 394);
+            Controls.Add(connectedControllerName);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(buttonDownRight);
+            Controls.Add(buttonDown);
+            Controls.Add(buttonDownLeft);
+            Controls.Add(buttonRight);
+            Controls.Add(buttonNeutral);
+            Controls.Add(buttonLeft);
+            Controls.Add(buttonUpRight);
+            Controls.Add(buttonUp);
+            Controls.Add(buttonUpLeft);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            RightToLeft = RightToLeft.No;
+            Text = "Shoryutrain";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonUpLeft;
+        private Button buttonUp;
+        private Button buttonUpRight;
+        private Button buttonLeft;
+        private Button buttonNeutral;
+        private Button buttonRight;
+        private Button buttonDownLeft;
+        private Button buttonDown;
+        private Button buttonDownRight;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem inputHandlerSelector;
+        private ToolStripMenuItem xinputMenuItem;
+        private ToolStripMenuItem directInputMenuItem;
+        private ToolStripMenuItem gameTypeSelector;
+        private ToolStripMenuItem streetFighterToolStripMenuItem;
+        private ToolStripMenuItem About;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private TextBox connectedControllerName;
+    }
+}
---
+++ shoryutrain/Form1.cs
@@ -0,0 +1,323 @@
+using SharpDX.DirectInput;
+using SharpDX.XInput;
+using System;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace shoryutrain
+{
+    public partial class Form1 : Form
+    {
+        private const int Deadzone = 17000; // Define a deadzone constant value
+        private DirectInput directInput;
+        private Joystick joystick;
+        private Controller xinputController; // Controller object for XInput
+        private System.Windows.Forms.Timer inputPollTimer;
+        private bool useDirectInput = false; // Default to XInput
+        public Form1()
+        {
+
+            InitializeComponent();
+            InitController();
+            InitializeTimer();
+        }
+
+        private void InitController()
+        {
+            try
+            {
+                if (useDirectInput)
+                {
+                    InitializeDirectInputController();
+                }
+                else
+                {
+                    InitializeXInputController();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while initializing the controller: {ex.Message}"); // Show error message if initialization fails
+            }
+        }
+
+        private void InitializeDirectInputController()
+        {
+            directInput = new DirectInput(); // Initialize DirectInput
+            var joystickGuid = Guid.Empty; // Initialize GUID for joystick
+
+            foreach (var deviceInstance in directInput.GetDevices(SharpDX.DirectInput.DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices)) // Enumerate all connected gamepads
+            {
+                joystickGuid = deviceInstance.InstanceGuid; // Get the GUID of the first gamepad
+                connectedControllerName.Text = "Connected controller: " + deviceInstance.InstanceName; // Set textbox text to the name of the connected controller
+                break; // Exit the loop once a gamepad is found
+            }
+
+            if (joystickGuid == Guid.Empty)
+            {
+                MessageBox.Show("There is no controller detected.");
+                connectedControllerName.Text = "There is no controller connected."; // Set textbox text if no gamepad is found
+                return;
+            }
+
+            joystick = new Joystick(directInput, joystickGuid); // Create a new Joystick object with the found GUID
+            joystick.Acquire(); // Acquire the joystick for use
+            xinputController = null; // Ensure XInput controller is not used
+        }
+
+        private void InitializeXInputController()
+        {
+            xinputController = new Controller(UserIndex.One); // Initialize the first XInput controller
+
+            if (xinputController.IsConnected)
+            {
+                string controllerName = GetXInputControllerName();
+                connectedControllerName.Text = "Connected controller: " + controllerName; // Set textbox text to the name of the connected controller
+            }
+            else
+            {
+                MessageBox.Show("There is no Xinput controller detected." + Environment.NewLine + "Try using DirectInput");
+                connectedControllerName.Text = "There is no controller connected."; // Set textbox text if no Xbox controller is connected
+                return;
+            }
+
+            joystick = null; // Ensure DirectInput joystick is not used
+        }
+
+        private string GetXInputControllerName()
+        {
+            // In the absence of a direct way to get the name, match against DirectInput devices
+            directInput = new DirectInput(); // Initialize DirectInput
+
+            foreach (var deviceInstance in directInput.GetDevices(SharpDX.DirectInput.DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices)) // Enumerate all connected gamepads
+            {
+                // Since exact matching is complex, a basic check against known GUIDs or properties could be done
+                // For example, check against common product GUIDs or other known identifiers
+                // This is a simplified approach and may need refinement for more accurate identification
+                return deviceInstance.InstanceName; // Return the name of the matched gamepad
+            }
+            return "Unknown XInput Controller"; // Return a default name if no match is found
+
+        }
+
+
+        private void InitializeTimer()
+        {
+            inputPollTimer = new System.Windows.Forms.Timer(); // Create a new Timer object
+            inputPollTimer.Interval = CalculateTimerInterval(60); // Set the timer interval to approximately 16 milliseconds (60 FPS)
+            inputPollTimer.Tick += InputPollTimer_Tick; // Add the event handler for the Tick event
+            inputPollTimer.Start(); // Start the timer
+        }
+        private int CalculateTimerInterval(int fps)
+        {
+            return (int)Math.Round(1000.0 / fps);
+        }
+
+
+        private void InputPollTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (useDirectInput)
+                {
+                    if (joystick == null)
+                    {
+                        return; // Exit if joystick is not initialized
+                    }
+
+                    joystick.Poll(); // Poll the joystick for its current state
+                    var state = joystick.GetCurrentState(); // Get the current state of the joystick
+
+                    int x = state.X; // Get the X axis value
+                    int y = state.Y; // Get the Y axis value
+                    bool dpadUp = false;
+                    bool dpadDown = false;
+                    bool dpadLeft = false;
+                    bool dpadRight = false;
+
+
+
+                    ResetButtonColors(); // Reset the colors of all buttons to their default
+
+                    UpdateButtonColors(x, y, dpadDown, dpadUp, dpadRight, dpadLeft); // Update button colors based on controller state
+                }
+                else
+                {
+                    if (xinputController == null || !xinputController.IsConnected)
+                    {
+                        return; // Exit if XInput controller is not initialized or not connected
+                    }
+
+                    var state = xinputController.GetState(); // Get the current state of the XInput controller
+                    int x = state.Gamepad.LeftThumbX; // Get the X axis value of the left thumbstick
+                    int y = state.Gamepad.LeftThumbY; // Get the Y axis value of the left thumbstick
+                    var buttons = state.Gamepad.Buttons;
+
+                    bool dpadUp = (buttons & GamepadButtonFlags.DPadUp) == GamepadButtonFlags.DPadUp;
+                    bool dpadDown = (buttons & GamepadButtonFlags.DPadDown) == GamepadButtonFlags.DPadDown;
+                    bool dpadRight = (buttons & GamepadButtonFlags.DPadRight) == GamepadButtonFlags.DPadRight;
+                    bool dpadLeft = (buttons & GamepadButtonFlags.DPadLeft) == GamepadButtonFlags.DPadLeft;
+
+
+
+                    ResetButtonColors(); // Reset the colors of all buttons to their default
+
+                    UpdateButtonColors(x, y, dpadDown, dpadUp, dpadRight, dpadLeft); // Update button colors based on controller state
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while polling the controller: {ex.Message}"); // Show error message if polling fails
+            }
+        }
+
+        private void UpdateButtonColors(int x, int y, bool dpadDown, bool dpadUp, bool dpadRight, bool dpadLeft)
+        {
+
+            y = -y; // Invert the Y-axis value
+
+            // Determine which button to highlight based on the X and Y axis values
+            if (y < -Deadzone || dpadUp)
+            {
+                if (x < -Deadzone || dpadLeft) // Top-left direction
+                {
+                    buttonUpLeft.BackColor = Color.Black;
+                    buttonUpLeft.ForeColor = Color.White;
+                }
+                else if (x > Deadzone || dpadRight) // Top-right direction
+                {
+                    buttonUpRight.BackColor = Color.Black;
+                    buttonUpRight.ForeColor = Color.White;
+                }
+                else // Up direction
+                {
+                    buttonUp.BackColor = Color.Black;
+                    buttonUp.ForeColor = Color.White;
+                }
+            }
+            else if (y > Deadzone || dpadDown)
+            {
+                if (x < -Deadzone || dpadLeft) // Bottom-left direction
+                {
+                    buttonDownLeft.BackColor = Color.Black;
+                    buttonDownLeft.ForeColor = Color.White;
+                }
+                else if (x > Deadzone || dpadRight) // Bottom-right direction
+                {
+                    buttonDownRight.BackColor = Color.Black;
+                    buttonDownRight.ForeColor = Color.White;
+                }
+                else // Down direction
+                {
+                    buttonDown.BackColor = Color.Black;
+                    buttonDown.ForeColor = Color.White;
+                }
+            }
+            else
+            {
+                if (x < -Deadzone || dpadLeft) // Left direction
+                {
+                    buttonLeft.BackColor = Color.Black;
+                    buttonLeft.ForeColor = Color.White;
+                }
+                else if (x > Deadzone || dpadRight) // Right direction
+                {
+                    buttonRight.BackColor = Color.Black;
+                    buttonRight.ForeColor = Color.White;
+                }
+                else // Center position
+                {
+                    buttonNeutral.BackColor = Color.Black;
+                    buttonNeutral.ForeColor = Color.White;
+                }
+            }
+        }
+
+
+
+        private void ResetButtonColors()
+        {
+            buttonUp.BackColor = SystemColors.Control;
+            buttonUp.ForeColor = SystemColors.ControlText;
+
+            buttonDown.BackColor = SystemColors.Control;
+            buttonDown.ForeColor = SystemColors.ControlText;
+
+            buttonLeft.BackColor = SystemColors.Control;
+            buttonLeft.ForeColor = SystemColors.ControlText;
+
+            buttonRight.BackColor = SystemColors.Control;
+            buttonRight.ForeColor = SystemColors.ControlText;
+
+            buttonNeutral.BackColor = SystemColors.Control;
+            buttonNeutral.ForeColor = SystemColors.ControlText;
+
+            buttonUpLeft.BackColor = SystemColors.Control;
+            buttonUpLeft.ForeColor = SystemColors.ControlText;
+
+            buttonUpRight.BackColor = SystemColors.Control;
+            buttonUpRight.ForeColor = SystemColors.ControlText;
+
+            buttonDownLeft.BackColor = SystemColors.Control;
+            buttonDownLeft.ForeColor = SystemColors.ControlText;
+
+            buttonDownRight.BackColor = SystemColors.Control;
+            buttonDownRight.ForeColor = SystemColors.ControlText;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void About_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void UpdateMenuSelection(ToolStripMenuItem selectedItem)
+        {
+            foreach (ToolStripMenuItem item in ((ToolStripMenuItem)MainMenuStrip.Items[0]).DropDownItems) // Iterate through all items in the settings menu
+            {
+                item.Checked = item == selectedItem; // Check the selected item and uncheck others
+            }
+        }
+
+
+        private void xinputMenuItem_Click(object sender, EventArgs e)
+        {
+            useDirectInput = false; // Set flag to use XInput
+            UpdateMenuSelection((ToolStripMenuItem)sender); // Update the menu selection
+            InitController(); // Reinitialize the controller to use XInput
+        }
+
+        private void directInputMenuItem_Click(object sender, EventArgs e)
+        {
+            useDirectInput = true; // Set flag to use DirectInput
+            UpdateMenuSelection((ToolStripMenuItem)sender); // Update the menu selection
+            InitController(); // Reinitialize the controller to use DirectInput
+
+        }
+
+        private void connectedControllerName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
